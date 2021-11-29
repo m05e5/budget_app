aaa5fb29da20109cfc26e297585088219c2c1b85
m05e5 <calebmoses1111@gmail.com>
2021-11-29 10:57:46 +0100

    setup devise, add models, controllers and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  get 'transactions/index'
+  get 'transactions/create'
+  get 'categories/index'
+  get 'categories/create'
+  devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+user = User.new
+user.name = 'admin'
+user.email = 'admin@mail.com'
+user.password = '111111'
+user.save!
